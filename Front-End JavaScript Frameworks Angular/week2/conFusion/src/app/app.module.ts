cb1b6bd0f90cb6377708b56cc913eb5089104c73
Somilbans <somil.bansal.1994@gmail.com>
2017-10-21 14:09:23 +0530

    Basic Service

---
+++ Front-End JavaScript Frameworks Angular/week2/conFusion/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
+  ],
+  providers: [DishService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

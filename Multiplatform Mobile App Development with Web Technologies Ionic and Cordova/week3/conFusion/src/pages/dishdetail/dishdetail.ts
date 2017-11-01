8ae18337ce472e206af15587f2ac3546d39a0e35
Somilbans <somil.bansal.1994@gmail.com>
2017-11-01 18:02:48 +0530

    Split Pane

---
+++ Multiplatform Mobile App Development with Web Technologies Ionic and Cordova/week3/conFusion/src/pages/dishdetail/dishdetail.ts
@@ -0,0 +1,95 @@
+import { Component, Inject } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController, ActionSheetController, ModalController } from 'ionic-angular';
+import { Dish } from '../../shared/dish';
+import { Comment } from '../../shared/comment';
+import { FavoriteProvider } from '../../providers/favorite/favorite';
+import{ CommentPage } from '../comment/comment';
+
+/**
+ * Generated class for the DishdetailPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-dishdetail',
+  templateUrl: 'dishdetail.html',
+})
+
+export class DishdetailPage {
+  dish: Dish;
+  errMess: string;
+  avgstars: string;
+  numcomments: number;
+
+  favorite: boolean;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    @Inject('BaseURL') private BaseURL,
+    private favoriteservice: FavoriteProvider,
+    private toastCtrl: ToastController,
+    public actionsheetCtrl: ActionSheetController,
+    private modalCtrl: ModalController) {
+      
+      this.dish = navParams.get('dish');
+      this.favorite = favoriteservice.isFavorite(this.dish.id);
+     
+      this.numcomments = this.dish.comments.length;
+      let total = 0;
+      this.dish.comments.forEach(comment => total += comment.rating );
+      this.avgstars = (total/this.numcomments).toFixed(2);
+  }
+
+  addToFavorites() {
+    console.log('Adding to Favorites', this.dish.id);
+    this.favorite = this.favoriteservice.addFavorite(this.dish.id);
+
+    this.toastCtrl.create({
+      message: 'Dish ' + this.dish.id + ' added as favorite successfully',
+      position: 'middle',
+      duration: 3000}).present();
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DishdetailPage');
+  }
+
+  openSheet() {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: 'Select Actions',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Add to Favorites',
+          handler: () => {
+            this.addToFavorites();
+          }
+        },
+        {
+          text: 'Add Comment',
+          handler: () => {
+            let modal = this.modalCtrl.create(CommentPage);
+            modal.onDidDismiss(
+              comment =>{
+                if(comment){
+                  this.dish.comments.push(comment);
+                }
+              }
+            )
+            modal.present();
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel', // will always sort to be on the bottom
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }  
+
+}

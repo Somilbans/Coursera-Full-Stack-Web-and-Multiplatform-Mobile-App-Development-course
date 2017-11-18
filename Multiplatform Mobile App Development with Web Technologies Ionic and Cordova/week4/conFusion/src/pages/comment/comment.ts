9e4a58dcb69ea78ce2e351dba98fb0e1d39adecf
Somilbans <somil.bansal.1994@gmail.com>
2017-11-18 12:43:33 +0530

    splash screen

---
+++ Multiplatform Mobile App Development with Web Technologies Ionic and Cordova/week4/conFusion/src/pages/comment/comment.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+
+
+/**
+ * Generated class for the CommentPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-comment',
+  templateUrl: 'comment.html',
+})
+export class CommentPage {
+  
+    commentForm: FormGroup;
+  
+    constructor(public navCtrl: NavController,
+      public navParams: NavParams,
+      public viewCtrl: ViewController,
+      private formbuilder: FormBuilder) {
+  
+      this.commentForm = this.formbuilder.group({
+        author: ['', Validators.required],
+        rating: 5,
+        comments: ['', Validators.required]
+      })
+    }
+  
+    ionViewDidLoad() {
+      console.log('ionViewDidLoad CommentPage');
+    }
+  
+    dismiss() {
+      this.viewCtrl.dismiss();
+    }
+  
+    onSubmit() {
+      console.log(this.commentForm.value);
+      this.viewCtrl.dismiss({
+        'author': this.commentForm.value.author,
+        'date': new Date().toISOString(),
+        'comment': this.commentForm.value.comments,
+        'rating': this.commentForm.value.rating
+  
+      });
+    }
+  
+  }
---
+++ Multiplatform Mobile App Development with Web Technologies Ionic and Cordova/week4/conFusion/src/shared/baseurl.ts
@@ -0,0 +1,1 @@
+export const baseURL = 'http://192.168.43.61:3000/';

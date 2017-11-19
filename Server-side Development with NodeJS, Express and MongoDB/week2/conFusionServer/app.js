0a160e0aaef6de553dbfc0200f201f8b76ecd09b
Somilbans <somil.bansal.1994@gmail.com>
2017-11-19 19:25:17 +0530

    Express Generator REST API

--- Server-side Development with NodeJS, Express and MongoDB/week2/conFusionServer/app.js
+++ Server-side Development with NodeJS, Express and MongoDB/week2/conFusionServer/app.js
@@ -5,8 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+
+
+
 var index = require('./routes/index');
 var users = require('./routes/users');
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
+
 
 var app = express();
 
@@ -24,6 +32,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

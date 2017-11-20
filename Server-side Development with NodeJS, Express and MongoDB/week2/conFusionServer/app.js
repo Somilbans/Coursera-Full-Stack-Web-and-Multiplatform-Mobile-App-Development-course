9ba2da1eadbf560f2831a1d1d4f773555d0594f1
Somilbans <somil.bansal.1994@gmail.com>
2017-11-20 17:49:18 +0530

    Express REST API with MongoDB and Mongoose Part 1

--- Server-side Development with NodeJS, Express and MongoDB/week2/conFusionServer/app.js
+++ Server-side Development with NodeJS, Express and MongoDB/week2/conFusionServer/app.js
@@ -6,15 +6,26 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 
-
-
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+const Dishes = require('./models/dishes');
+// Connection URL
+const url = 'mongodb://localhost:27017/conFusion';
+
+const connect = mongoose.connect(url, {
+  useMongoClient: true,
+});
+
+connect.then((db) => {
+ console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
 
 var app = express();
 

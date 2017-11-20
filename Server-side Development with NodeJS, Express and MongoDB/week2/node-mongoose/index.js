4b0774e224d455c99d207f78f9cd40d8612c6b0a
Somilbans <somil.bansal.1994@gmail.com>
2017-11-20 10:09:48 +0530

    Mongoose part 2

--- Server-side Development with NodeJS, Express and MongoDB/week2/node-mongoose/index.js
+++ Server-side Development with NodeJS, Express and MongoDB/week2/node-mongoose/index.js
@@ -10,13 +10,26 @@ const connect = mongoose.connect(url, {
 
 connect.then((db) => {
     console.log('Connected correctly to server');
-    var newDish = Dishes({
-        name: 'Uthappizza',
-        description: 'test'
-    });
-    newDish.save().then((dish) => {
+    Dishes.create({
+        name: 'Uthapizza',
+        description: 'Test'
+    }).then((dish) => {
         console.log(dish);
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },
+        {
+            new: true
+        })
+        .exec();
+    }).then((dish) => {
+        console.log(dish);
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+         });
+        return dish.save();
     }).then((dishes) => {
             console.log(dishes);
             return db.collection('dishes').drop();

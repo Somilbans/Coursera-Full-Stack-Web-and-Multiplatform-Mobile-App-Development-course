6305508addf3847632c78072a2e7a45da8dbbcfb
Somilbans <somil.bansal.1994@gmail.com>
2017-11-21 20:54:37 +0530

    passport jwt part 2

--- Server-side Development with NodeJS, Express and MongoDB/week3/conFusionServer/app.js
+++ Server-side Development with NodeJS, Express and MongoDB/week3/conFusionServer/app.js
@@ -45,33 +45,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
- }));
+
  app.use(passport.initialize());
- app.use(passport.session());
+ 
  
  app.use('/', index);
  app.use('/users', users);
 
- function auth (req, res, next) {
-  console.log(req.user);
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
-  }
-  else {
-    next();
-  }
- }
-app.use(auth);
- 
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);

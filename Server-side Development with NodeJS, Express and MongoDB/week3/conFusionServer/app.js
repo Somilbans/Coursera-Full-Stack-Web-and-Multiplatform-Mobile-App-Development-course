4adabd9f7962af5ea1b957acae574f0fef5f00be
Somilbans <somil.bansal.1994@gmail.com>
2017-12-03 21:02:55 +0530

    File Upload

--- Server-side Development with NodeJS, Express and MongoDB/week3/conFusionServer/app.js
+++ Server-side Development with NodeJS, Express and MongoDB/week3/conFusionServer/app.js
@@ -16,13 +16,14 @@ var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
-
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 const Dishes = require('./models/dishes');
 const Leaders = require('./models/leaders');
 const Promotions = require('./models/promotions');
+
 // Connection URL
 const url = config.mongoUrl;
 
@@ -68,6 +69,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
